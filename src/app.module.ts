56aa8bc5c77b2355466147d90243d414e3071606
Lucas-Henry <henrylucas347@gmail.com>
2025-10-22 03:13:33 -0300

    fix: update module imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,25 +1,19 @@
 import { Module } from '@nestjs/common';
-
-/* import { ConfigModule } from '@nestjs/config';
-* import { DiscordModule } from './discord/discord.module';
-* import { SearchModule } from './search/search.module';
-* import { AiModule } from './infrastructure/ai/ai.module';
-* import envValidation } from '.infrastructure/config/env.validation';
-*/
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { DiscordModule } from './infrastructure/discord/discord.module';
+import { SearchModule } from './infrastructure/search/search.module';
+import { AiModule } from './infrastructure/ai/ai.module';
+import { envValidation } from './infrastructure/config/env.validation';
 
 @Module({
-  imports: [],
-  /* ConfigModule.forRoot({
-  * isGlobal: true,
-  * validate: envValidation,
-  * }),
-  * DiscordModule,
-  * SearchModule,
-  * AiModule,
-  */
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validate: envValidation,
+    }),
+    DiscordModule,
+    SearchModule,
+    AiModule,
+  ],
 })
 export class AppModule { }
